3b70d99b948607b27b5d4f8a9437a979c6f63182
ugowe <joe@spheares.com>
2016-09-09 12:59:49 -0400

    Initial commit

---
+++ Gram/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  Gram
+//
+//  Created by Ugowe on 7/20/16.
+//  Copyright © 2016 Ugowe. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var textView: UITextView!
+    
+    var imagePicker: UIImagePickerController!
+    private lazy var client : ClarifaiClient = ClarifaiClient(appID: clarifaiClientID, appSecret: clarifaiClientSecret)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func takePhoto(sender: AnyObject) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func selectPhoto(sender: AnyObject) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageView.contentMode = .ScaleAspectFit
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        UIImageWriteToSavedPhotosAlbum(imageView.image!, self, nil, nil)
+
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            // The user picked an image. Send it to Clarifai for recognition.
+            imageView.image = image
+            textView.text = "Recognizing..."
+            recognizeImage(image)
+            
+        }
+    }
+    
+    func recognizeImage(image: UIImage!) {
+        // Scale down the image. This step is optional. However, sending large images over the
+        // network is slow and does not significantly improve recognition performance.
+        let size = CGSizeMake(320, 320 * image.size.height / image.size.width)
+        UIGraphicsBeginImageContext(size)
+        image.drawInRect(CGRectMake(0, 0, size.width, size.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // Encode as a JPEG.
+        let jpeg = UIImageJPEGRepresentation(scaledImage, 0.9)!
+        
+        // Send the JPEG to Clarifai for standard image tagging.
+        client.recognizeJpegs([jpeg]) {
+            (results: [ClarifaiResult]?, error: NSError?) in
+            if error != nil {
+                print("Error: \(error)\n")
+                self.textView.text = "Sorry, there was an error recognizing your image."
+            } else {
+                self.textView.text = "Tags:\n" + results![0].tags.joinWithSeparator(", ")
+            }
+        }
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafePointer<Void>) {
+        if error == nil {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+        } else {
+            let ac = UIAlertController(title: "Save error", message: error?.localizedDescription, preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+        }
+    }
+    
+}
+
